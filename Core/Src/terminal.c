9698c2a705422606515d16dc919f5f48dd64a6d2
Nick Butsky <nbutskyi@gmail.com>
2025-05-11 19:57:39 +0300

    more refactoring

--- Core/Src/terminal.c
+++ Core/Src/terminal.c
@@ -7,17 +7,6 @@ char rxCharacter;
 
 void initialiseTerminal() { HAL_UART_Receive_IT(&huart2, (uint8_t *)&rxCharacter, 1); }
 
-void printEverything() {
-  while (putIndex - getIndex) {
-    char character = '\0';
-    if (putIndex != getIndex) {
-      character = buffer[getIndex];
-      getIndex = BUFFER_LENGTH > getIndex + 1 ? getIndex + 1 : 0;
-    }
-    __io_putchar(character);
-  }
-}
-
 int __io_putchar(int ch) {
   const uint8_t offset = 7;
   while (1) {
@@ -27,6 +16,17 @@ int __io_putchar(int ch) {
   }
   USART2->DR = (uint8_t)ch;
   return ch;
+}
+
+void printEverything() {
+  while (putIndex - getIndex) {
+    char character = '\0';
+    if (putIndex != getIndex) {
+      character = buffer[getIndex];
+      getIndex = BUFFER_LENGTH > getIndex + 1 ? getIndex + 1 : 0;
+    }
+    __io_putchar(character);
+  }
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
