c3047c8d3a62dac9383dedbe4e8f6d6fefce70c0
Nick Butsky <nbutskyi@gmail.com>
2025-05-12 14:52:18 +0300

    fix plugin

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,6 @@ export default defineConfig({
 	plugins: [tailwindcss(), injectCssAsStyleTag()],
 });
 
-function escapeRegex(string) {
-	return string.replace(/[/\-\\^$*+?.()|[\]{}]/g, "\\$&");
-}
-
 function injectCssAsStyleTag() {
 	return {
 		name: "inject-css-as-style-tags",
@@ -17,23 +13,26 @@ function injectCssAsStyleTag() {
 			order: "post",
 			handler: (html, context) => {
 				const tags = [];
-				const output = Object.values(context.bundle).filter((output) =>
+				for (const output of Object.values(context.bundle).filter((output) =>
 					output.fileName.endsWith(".css"),
-				)[0];
-				let outputHtml = html;
-				if (output.type === "asset" && typeof output.source === "string") {
+				)) {
+					if (!(output.type === "asset" && typeof output.source === "string")) {
+						continue;
+					}
 					tags.push({
 						tag: "style",
 						children: output.source,
 						injectTo: "head",
 					});
-					const fileNameRegExp = RegExp(
-						`<link.*href=".*${escapeRegex(output.fileName)}".*\\/?>`,
-						"gmi",
+					html = html.replaceAll(
+						RegExp(
+							`<link.*href=".*${output.fileName.replace(/[/\-\\^$*+?.()|[\]{}]/g, "\\$&")}".*\\/?>`,
+							"gmi",
+						),
+						"",
 					);
-					outputHtml = html.replaceAll(fileNameRegExp, "");
 				}
-				return { outputHtml, tags };
+				return { html, tags };
 			},
 		},
 	};
