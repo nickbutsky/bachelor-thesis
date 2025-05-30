afe217915bac45dd466cca66026aab22f819e0aa
Nick Butsky <nbutskyi@gmail.com>
2025-05-30 11:31:24 +0300

    improve

--- Core/Src/dht11.c
+++ Core/Src/dht11.c
@@ -1,12 +1,12 @@
 #include "dht11.h"
 
-enum { DATA_LENGTH = 40, MAX_TICK_NUMBER = 1000 };
-
-static inline uint8_t readData(uint16_t *data) {
+DHT11 getDht11() {
   const uint16_t resetDelay = 500;
   const uint8_t setDelay = 20;
   uint16_t tickNumber = 0;
-  const uint8_t five = 5;
+  enum { DATA_LENGTH = 40 };
+  const uint16_t maxTickNumber = 1000;
+  uint16_t data[DATA_LENGTH] = {0};
   HAL_Delay(resetDelay);
   HAL_GPIO_WritePin(DHT11_GPIO_Port, DHT11_Pin, GPIO_PIN_RESET);
   HAL_Delay(setDelay);
@@ -16,47 +16,39 @@ static inline uint8_t readData(uint16_t *data) {
     GPIO_PinState waitValue = i % 2 ? GPIO_PIN_SET : GPIO_PIN_RESET;
     while (HAL_GPIO_ReadPin(DHT11_GPIO_Port, DHT11_Pin) == waitValue) {
       ++tickNumber;
-      if (tickNumber >= MAX_TICK_NUMBER) {
+      if (tickNumber >= maxTickNumber) {
         HAL_GPIO_WritePin(DHT11_GPIO_Port, DHT11_Pin, GPIO_PIN_SET);
-        return 1;
+        return (DHT11){DHT11_NO_CONNECTION};
       }
     }
-    if (waitValue == GPIO_PIN_SET && i >= five) {
-      data[(i - five) / 2] = tickNumber;
+    if (waitValue == GPIO_PIN_SET && i >= 4) {
+      data[(i - 4) / 2] = tickNumber;
     }
   }
   HAL_GPIO_WritePin(DHT11_GPIO_Port, DHT11_Pin, GPIO_PIN_SET);
-  return 0;
-}
 
-DHT11 getDht11() {
-  uint16_t data[DATA_LENGTH] = {0};
-  if (readData(data)) {
-    return (DHT11){DHT11_NO_CONNECTION};
-  }
-
-  uint16_t maxTickNumber = 0;
-  uint16_t minTickNumber = MAX_TICK_NUMBER;
+  uint16_t maxFoundTickNumber = 0;
+  uint16_t minFoundTickNumber = maxTickNumber;
   for (uint32_t i = 0; i < DATA_LENGTH; ++i) {
-    if (data[i] > maxTickNumber) {
-      maxTickNumber = data[i];
+    if (data[i] > maxFoundTickNumber) {
+      maxFoundTickNumber = data[i];
     }
-    if (data[i] < minTickNumber) {
-      minTickNumber = data[i];
+    if (data[i] < minFoundTickNumber) {
+      minFoundTickNumber = data[i];
     }
   }
-  if (maxTickNumber < minTickNumber) {
+  if (maxFoundTickNumber < minFoundTickNumber) {
     return (DHT11){DHT11_NO_CONNECTION};
   }
 
   enum { BUFFER_LENGTH = 5 };
   uint8_t buffer[BUFFER_LENGTH] = {0};
-  uint16_t maxTickNumberMinTickNumberMean = (maxTickNumber + minTickNumber) / 2;
+  uint16_t extremeTickNumberMean = (maxFoundTickNumber + minFoundTickNumber) / 2;
   for (uint32_t i = 0; i < DATA_LENGTH; ++i) {
     const uint8_t eight = 8;
     uint8_t bufferIndex = i / eight;
     buffer[bufferIndex] *= 2;
-    if (data[i] > maxTickNumberMinTickNumberMean) {
+    if (data[i] > extremeTickNumberMean) {
       buffer[bufferIndex] |= (uint8_t)1;
     }
   }
