c267265ab7376a6af8185b9de54eee7ca7bf8010
Nick Butsky <nbutskyi@gmail.com>
2025-05-31 18:14:40 +0300

    rename variable

--- Core/Src/esp8266.c
+++ Core/Src/esp8266.c
@@ -67,10 +67,10 @@ Esp8266 runEsp8266() {
   return (Esp8266){-1};
 }
 
-static inline void sendHttpResponse(const Esp8266 *esp8266Ptr, const char *content) {
+static inline void sendHttpResponse(const Esp8266 *esp8266Ptr, const char *body) {
   enum { RESPONSE_MAX_LENGTH = 2048 };
   char response[RESPONSE_MAX_LENGTH] = {0};
-  (void)sprintf(response, "HTTP/1.1 200 OK\ncontent-type:%s\n\n%s", esp8266Ptr->contentType, content);
+  (void)sprintf(response, "HTTP/1.1 200 OK\ncontent-type:%s\n\n%s", esp8266Ptr->contentType, body);
   char command[COMMAND_MAX_LENGTH] = {0};
   (void)sprintf(command, "AT+CIPSEND=%lu,%d\r\n", esp8266Ptr->linkId, strlen(response));
   const uint8_t delay = 20;
@@ -82,7 +82,7 @@ static inline void sendHttpResponse(const Esp8266 *esp8266Ptr, const char *conte
 
 void handleRequest(const Esp8266 *esp8266Ptr, const DHT11 *dht11Ptr, uint32_t photoresistorValue) {
   enum { CONTENT_MAX_LENGTH = 2048 };
-  char content[CONTENT_MAX_LENGTH] = {0};
+  char body[CONTENT_MAX_LENGTH] = {0};
   const char *template = NULL;
   if (!strcmp(esp8266Ptr->contentType, "application/json")) {
     template = "{\"th\":{\"ok\":%d,\"t\":%d,\"h\":%d},\"l\":%ld}";
@@ -92,6 +92,6 @@ void handleRequest(const Esp8266 *esp8266Ptr, const DHT11 *dht11Ptr, uint32_t ph
     closeConnection(esp8266Ptr->linkId);
     return;
   }
-  (void)sprintf(content, template, !dht11Ptr->status, dht11Ptr->temperature, dht11Ptr->humidity, photoresistorValue);
-  sendHttpResponse(esp8266Ptr, content);
+  (void)sprintf(body, template, !dht11Ptr->status, dht11Ptr->temperature, dht11Ptr->humidity, photoresistorValue);
+  sendHttpResponse(esp8266Ptr, body);
 }
